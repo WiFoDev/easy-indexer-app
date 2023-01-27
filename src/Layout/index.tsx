69dba0b09271599d5f22a228436a3979bd5d3d12
WiFoDev <lou81804@gmail.com>
2023-01-26 23:17:47 -0500

    add get token balances UI

--- src/Layout/index.tsx
+++ src/Layout/index.tsx
@@ -7,23 +7,23 @@ type LayoutProps = {
 
 export const Layout = ({children}: LayoutProps) => {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex min-h-screen flex-col">
       <Head>
-        <title>APP</title>
+        <title>Easy Indexer App</title>
         <meta content="The next generation app" name="My App" />
         <link href="/favicon.ico" rel="icon" />
       </Head>
       <header className="sticky top-0 z-20 w-full bg-background/50 backdrop-blur-sm">
-        <nav className="flex h-16 mx-auto max-w-screen-standar items-center justify-end gap-2 pl-[max(env(safe-area-inset-left),1.5rem)] pr-[max(env(safe-area-inset-right),1.5rem)]">
-          <div className="mr-auto text-xl">My APP</div>
+        <nav className="mx-auto flex h-16 max-w-screen-standar items-center justify-end gap-2 pl-[max(env(safe-area-inset-left),1.5rem)] pr-[max(env(safe-area-inset-right),1.5rem)]">
+          <div className="mr-auto text-xl">Easy Indexer App</div>
           <ul className="text-lg">Navigation List For The Page</ul>
         </nav>
       </header>
-      <main className="flex-1 mx-auto w-full grid max-w-screen-standar pl-[max(env(safe-area-inset-left),1.5rem)] pr-[max(env(safe-area-inset-right),1.5rem)]">
+      <main className="mx-auto grid w-full max-w-screen-standar flex-1 pl-[max(env(safe-area-inset-left),1.5rem)] pr-[max(env(safe-area-inset-right),1.5rem)]">
         {children}
       </main>
       <footer className="relative pb-[env(safe-area-inset-bottom)]">
-        <div className="mx-auto max-w-screen-standar py-6 flex justify-center pl-[max(env(safe-area-inset-left),1.5rem)] pr-[max(env(safe-area-inset-right),1.5rem)]">
+        <div className="mx-auto flex max-w-screen-standar justify-center py-6 pl-[max(env(safe-area-inset-left),1.5rem)] pr-[max(env(safe-area-inset-right),1.5rem)]">
           Made with ❤️ by WiFo
         </div>
       </footer>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,50 @@
 import type {NextPage} from "next";
 
+import {Alchemy, Network} from "alchemy-sdk";
+import {FormEventHandler, useEffect, useRef, useState} from "react";
+
+const settings = {
+  apiKey: "oqn6yIU_bcHgkSNXEycAv-Ikr5OYRdP-",
+  network: Network.ETH_MAINNET,
+};
+
 const Home: NextPage = () => {
-  return <section>Welcome</section>;
+  const [value, setValue] = useState("");
+  const alchemySDKRef = useRef<Alchemy | undefined>();
+
+  useEffect(() => {
+    if (!alchemySDKRef.current) {
+      alchemySDKRef.current = new Alchemy(settings);
+    }
+  }, []);
+
+  const submitHandler: FormEventHandler = (e) => {
+    e.preventDefault();
+    if (value.trim().length === 0 || !alchemySDKRef.current) {
+      return;
+    }
+
+    alchemySDKRef.current.core
+      .getTokenBalances(value)
+      .then(console.log);
+  };
+
+  return (
+    <section className="flex flex-col items-center gap-10">
+      <h1 className="mt-10 max-w-3xl text-center text-6xl font-bold">
+        The best way to track your tokens
+      </h1>
+      <form className="w-1/2" onSubmit={submitHandler}>
+        <input
+          className="w-full rounded-xl p-3 text-background outline-none"
+          placeholder="Enter a valid address"
+          type="text"
+          value={value}
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </form>
+    </section>
+  );
 };
 
 export default Home;
